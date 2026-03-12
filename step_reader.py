91187a4faabb4990bb064c87b1781d2e71e06954
Maheshadhi <109026387+Maheshadhi@users.noreply.github.com>
2026-03-12 15:40:21 +0530

    Update step_reader.py

--- step_reader.py
+++ step_reader.py
@@ -1,40 +1,21 @@
-try:
-    from OCC.Core.STEPControl import STEPControl_Reader
-    from OCC.Core.Bnd import Bnd_Box
-    from OCC.Core.BRepBndLib import brepbndlib_Add
-    from OCC.Core.GProp import GProp_GProps
-    from OCC.Core.BRepGProp import brepgprop_VolumeProperties
-    OCC_AVAILABLE = True
-except:
-    OCC_AVAILABLE = False
-
+import trimesh
 
 def extract_step_dimensions(file_path):
 
-    if not OCC_AVAILABLE:
-        raise Exception("STEP processing not supported on this server")
+    mesh = trimesh.load(file_path)
 
-    step_reader = STEPControl_Reader()
-    status = step_reader.ReadFile(file_path)
+    bounds = mesh.bounds
 
-    if status != 1:
-        raise Exception("STEP file could not be read")
-
-    step_reader.TransferRoots()
-    shape = step_reader.Shape()
-
-    bbox = Bnd_Box()
-    brepbndlib_Add(shape, bbox)
-
-    xmin, ymin, zmin, xmax, ymax, zmax = bbox.Get()
+    xmin, ymin, zmin = bounds[0]
+    xmax, ymax, zmax = bounds[1]
 
     length = xmax - xmin
     width = ymax - ymin
     thickness = zmax - zmin
 
-    props = GProp_GProps()
-    brepgprop_VolumeProperties(shape, props)
+    volume = mesh.volume
 
+    return length, width, thickness, volume
     volume = props.Mass()
 
     return length, width, thickness, volume
