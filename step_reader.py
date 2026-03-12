954873ff7ad45230b03fcd7354a558509c4d7857
Maheshadhi <109026387+Maheshadhi@users.noreply.github.com>
2026-03-12 15:34:46 +0530

    Update step_reader.py

--- step_reader.py
+++ step_reader.py
@@ -1,11 +1,18 @@
-from OCC.Core.STEPControl import STEPControl_Reader
-from OCC.Core.Bnd import Bnd_Box
-from OCC.Core.BRepBndLib import brepbndlib_Add
-from OCC.Core.GProp import GProp_GProps
-from OCC.Core.BRepGProp import brepgprop_VolumeProperties
+try:
+    from OCC.Core.STEPControl import STEPControl_Reader
+    from OCC.Core.Bnd import Bnd_Box
+    from OCC.Core.BRepBndLib import brepbndlib_Add
+    from OCC.Core.GProp import GProp_GProps
+    from OCC.Core.BRepGProp import brepgprop_VolumeProperties
+    OCC_AVAILABLE = True
+except:
+    OCC_AVAILABLE = False
 
 
 def extract_step_dimensions(file_path):
+
+    if not OCC_AVAILABLE:
+        raise Exception("STEP processing not supported on this server")
 
     step_reader = STEPControl_Reader()
     status = step_reader.ReadFile(file_path)
@@ -16,7 +23,6 @@ def extract_step_dimensions(file_path):
     step_reader.TransferRoots()
     shape = step_reader.Shape()
 
-    # Bounding box
     bbox = Bnd_Box()
     brepbndlib_Add(shape, bbox)
 
@@ -26,7 +32,6 @@ def extract_step_dimensions(file_path):
     width = ymax - ymin
     thickness = zmax - zmin
 
-    # Volume
     props = GProp_GProps()
     brepgprop_VolumeProperties(shape, props)
 
